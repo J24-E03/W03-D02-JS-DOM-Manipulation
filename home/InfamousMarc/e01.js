c51de6e6763bf2b803b77ab429a81b9757e30f00
Marcel Saegebarth <marcel.saegebarth@dci-student.org>
2024-10-15 13:51:57 +0200

    Implement counter reset

--- home/InfamousMarc/e01.js
+++ home/InfamousMarc/e01.js
@@ -4,8 +4,10 @@ class Counter {
         this.counter = document.querySelector("#counter");
         this.buttonIncrement = document.querySelector("#increment");
         this.buttonDecrement = document.querySelector("#decrement");
+        this.buttonReset = document.querySelector("#reset");
         this.buttonIncrement.addEventListener("click", this.increment.bind(this));
         this.buttonDecrement.addEventListener("click", this.decrement.bind(this));
+        this.buttonReset.addEventListener("click", this.reset.bind(this));
     }
 
     increment() {
@@ -19,6 +21,11 @@ class Counter {
         }
 
         this.count--;
+        this.counter.textContent = this.count;
+    }
+
+    reset() {
+        this.count = 0;
         this.counter.textContent = this.count;
     }
 }
